ef4f83ef3810face827252a8033e5689ab41ad9b
GrosvCorp3000 <grosvenor@gmail.com>
2014-02-04 12:23:02 -0500

    feb 4th commit
    added more to user key switch,, escape key code, added variables for
    dynamic memory

--- jan22/consolelineedit.cpp
+++ jan22/consolelineedit.cpp
@@ -1,4 +1,5 @@
 #include <cstring>
+#include "keys.h"
 #include "console.h"
 #include "consoleplus.h"
 
@@ -62,19 +63,113 @@ int columnCheck()
 
 int edit(char *str, int row, int col, int fieldLength, int maxStrLength, bool* insertMode, int* strOffset, int* curPosition)
 {
-	
-	if(*strOffset > maxStrLength)
-		*strOffset = maxStrLength;
+	int userKey=0;
+	int returnValue = 0;
+	int strLen = strlen(str);
 
+	//get the key pressed by the user, using the overloaded input operator
+	cio::console>> userKey;
+
+	//if the string offset is greater than the maximum length of the string,
+	//set it to that that max length
+	if(*strOffset > strLen)
+		*strOffset = strLen;
+
+
+	//if string offset is NULL, set it to zero
 	if(*strOffset==NULL)
 		*strOffset = 0;
 
+	//
 	if(*curPosition > fieldLength)
 		*curPosition = fieldLength;
 	
 	if(*curPosition==NULL)
 		*curPosition;
 
+	char* tempStr = NULL;
+		char* tempStr = new char[strLen+1];
+		strcpy(tempStr,str);
+	
+	//If the user pushes any of the following keys (as defined by keys.h), editing ends.
+	switch(userKey)
+	{
+		case ENTER:
+			returnValue=ENTER;
+			break;
 
-	return 0;
+		case TAB:
+			returnValue=TAB;
+			break;
+
+		case UP:
+			returnValue=UP;
+			break;
+
+		case DOWN:
+			returnValue=DOWN;
+			break;
+
+		case PGUP:
+			returnValue=PGUP;
+			break;				
+
+		case PGDN:
+			returnValue=PGDN;
+			break;
+
+		case F(1):
+			returnValue=F(1);
+			break;
+
+		case F(2):
+			returnValue=F(2);
+			break;
+
+		case F(3):
+			returnValue=F(3);
+			break;
+
+		case F(4):
+			returnValue=F(4);
+			break;
+
+		case F(5):
+			returnValue=F(5);
+			break;
+
+		case F(6):
+			returnValue=F(6);
+			break;
+
+		case F(7):
+			returnValue=F(7);
+			break;
+
+		case F(8):
+			returnValue=F(8);
+			break;
+
+		case F(9):
+			returnValue=F(9);
+			break;
+
+		case F(10):
+			returnValue=F(10);
+			break;
+
+		case F(11):
+			returnValue=F(11);
+			break;
+
+		case F(12):
+			returnValue=F(12);
+			break;
+	}
+	
+	if(userKey==ESCAPE)
+		returnValue = ESCAPE;
+		strcpy(str,tempStr);
+
+	return returnValue;
 }
