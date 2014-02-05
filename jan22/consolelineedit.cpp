eb74116cce5b19d5a672753d09e90034eff9d755
GrosvCorp3000 <grosvenor@gmail.com>
2014-02-05 01:40:32 -0500

    february 4th/5th
    continued adding keys to edit function, worked over variables at top as
    well

--- jan22/consolelineedit.cpp
+++ jan22/consolelineedit.cpp
@@ -57,6 +57,8 @@ int columnCheck()
 	cio::console.getPosition(urRow, urCol);
 	if(urCol==totCol)
 		endOfScreen = 1;
+	if(urCol < 0)
+		endOfScreen = -1;
 
 	return endOfScreen;
 }
@@ -66,110 +68,200 @@ int edit(char *str, int row, int col, int fieldLength, int maxStrLength, bool* i
 	int userKey=0;
 	int returnValue = 0;
 	int strLen = strlen(str);
+	int flag = 0;
+	
+	char* tempStr;
+	tempStr = new char[strLen+1];
+	strcpy(tempStr,str);
 
 	//get the key pressed by the user, using the overloaded input operator
 	cio::console>> userKey;
 
 	//if the string offset is greater than the maximum length of the string,
 	//set it to that that max length
-	if(*strOffset > strLen)
-		*strOffset = strLen;
-
-
+	if(*strOffset > maxStrLength)
+		*strOffset = maxStrLength;
+	
 	//if string offset is NULL, set it to zero
 	if(*strOffset==NULL)
 		*strOffset = 0;
 
-	//
-	if(*curPosition > fieldLength)
-		*curPosition = fieldLength;
+	//if cursors position is greater than or equal to the field length, set it to field length minus 1
+	if(*curPosition >= fieldLength)
+		*curPosition = fieldLength-1;
 	
-	if(*curPosition==NULL)
-		*curPosition;
-
-	char* tempStr = NULL;
-		char* tempStr = new char[strLen+1];
-		strcpy(tempStr,str);
-	
-	//If the user pushes any of the following keys (as defined by keys.h), editing ends.
-	switch(userKey)
+		
+	//put loop here
+	if(userKey >= ' ' && userKey <= '~')
 	{
-		case ENTER:
-			returnValue=ENTER;
-			break;
-
-		case TAB:
-			returnValue=TAB;
-			break;
-
-		case UP:
-			returnValue=UP;
-			break;
-
-		case DOWN:
-			returnValue=DOWN;
-			break;
-
-		case PGUP:
-			returnValue=PGUP;
-			break;				
-
-		case PGDN:
-			returnValue=PGDN;
-			break;
-
-		case F(1):
-			returnValue=F(1);
-			break;
-
-		case F(2):
-			returnValue=F(2);
-			break;
-
-		case F(3):
-			returnValue=F(3);
-			break;
-
-		case F(4):
-			returnValue=F(4);
-			break;
-
-		case F(5):
-			returnValue=F(5);
-			break;
-
-		case F(6):
-			returnValue=F(6);
-			break;
-
-		case F(7):
-			returnValue=F(7);
-			break;
-
-		case F(8):
-			returnValue=F(8);
-			break;
-
-		case F(9):
-			returnValue=F(9);
-			break;
-
-		case F(10):
-			returnValue=F(10);
-			break;
-
-		case F(11):
-			returnValue=F(11);
-			break;
-
-		case F(12):
-			returnValue=F(12);
-			break;
 	}
-	
-	if(userKey==ESCAPE)
+
+	else if(userKey==ESCAPE)
+	{
 		returnValue = ESCAPE;
 		strcpy(str,tempStr);
+	}
+
+	else
+	{
+	//If the user pushes any of the following keys (as defined by keys.h), editing ends.
+		switch(userKey)
+		{
+			case ENTER:
+				{
+					returnValue=ENTER;
+					flag = 1;
+					break;
+				}
+
+			case TAB:
+				{
+					returnValue=TAB;
+					flag=1;
+					break;
+				}
+
+			case UP:
+					returnValue=UP;
+					flag=1;
+					break;
+
+			case DOWN:
+				{	
+					returnValue=DOWN;
+					flag=1;
+					break;
+				}
+
+			case PGUP:
+				{
+					returnValue=PGUP;
+					flag=1;
+					break;				
+				}
+
+			case PGDN:
+				{
+					returnValue=PGDN;
+					flag=1;
+					break;
+				}
+
+			case F(1):
+				{
+					returnValue=F(1);
+					flag=1;
+					break;
+				}
+
+			case F(2):
+				{
+					returnValue=F(2);
+					flag=1;
+					break;
+				}
+
+			case F(3):
+				{
+					returnValue=F(3);
+					flag=1;
+					break;
+				}
+
+			case F(4):
+				{
+					returnValue=F(4);
+					flag=1;
+					break;
+				}
+
+			case F(5):
+				{
+					returnValue=F(5);
+					flag=1;
+					break;
+				}
+
+			case F(6):
+				{
+					returnValue=F(6);
+					flag=1;
+					break;
+				}
+			case F(7):
+				{
+					returnValue=F(7);
+					flag=1;
+					break;
+				}
+
+			case F(8):
+				{
+					returnValue=F(8);
+					flag=1;
+					break;
+				}
+			case F(9):
+				{
+					returnValue=F(9);
+					flag=1;
+					break;
+				}
+
+			case F(10):
+				{
+					returnValue=F(10);	
+					flag=1;
+					break;
+				}
+
+			case F(11):
+				{
+					returnValue=F(11);
+					flag=1;
+					break;
+				}
+
+			case F(12):
+				{
+					returnValue=F(12);
+					flag=1;
+					break;
+				}
+		}
+
+		if(userKey==LEFT && columnCheck()!=1 && (*curPosition < fieldLength-2))
+		{
+			cio::console.setPosition(row, col +1);
+			if(*strOffset < maxStrLength)
+				*strOffset += 1;
+		}
+
+		if(userKey==RIGHT && columnCheck()!=-1 && (*curPosition > *str))
+		{
+			cio::console.setPosition(row, col-1);
+			if(*strOffset < 0);
+				*strOffset -= 1;
+		}
+
+		if(userKey==HOME)
+		{
+			int startCol = *str-*curPosition;	
+			cio::console.setPosition(row, startCol);
+			if(*strOffset !=0)
+				*strOffset=0;
+		}
+
+		if(userKey==END)
+		{
+			
+			cio::console.setPosition(row,);
+			if(*strOffset < maxStrLength)
+				*strOffset = maxStrLength;
+			
+		}
+
+	}		
 
 	return returnValue;
 }
